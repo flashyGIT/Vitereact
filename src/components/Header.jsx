ffb693b06f2c6db24220bf9ae853161c831d8082
flashyGIT <xavierqbeekwilder@abbey.bac.edu>
2026-09-23 21:54:56 +0000

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+function Header() {
+return (
+<header className="header">
+<h1>Vinyl Records</h1>
+
+  <nav>
+    <a href="/">Home</a>
+    <a href="#products">Products</a>
+    <a href="#contact">Contact</a>
+  </nav>
+</header>
+
+
+);
+}
+
+export default Header;
