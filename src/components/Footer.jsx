aa2cf7fccdb0dd6249d1679de395f369039cedfe
flashyGIT <xavierqbeekwilder@abbey.bac.edu>
2026-09-23 21:56:19 +0000

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+return (
+<footer className="footer" id="contact">
+<p>© 2026 Vinyl Records. All rights reserved.</p>
+</footer>
+);
+}
+
+export default Footer;
